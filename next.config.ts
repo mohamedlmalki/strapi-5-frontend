0c76c5d42267be503694e59cdcb898dd0f310c4d
mohamedlmalki <mohamedlmalki@yahoo.com>
2025-03-11 02:32:52 +0000

    first commit

---
+++ next.config.ts
@@ -0,0 +1,21 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "1337",
+        pathname: "/uploads/**/*",
+      },
+      {
+        protocol: "https",
+        hostname: "luminous-addition-c153b007fc.media.strapiapp.com",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
